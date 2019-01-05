8f65caa518b156ff513c24bd30e61a610e38209f
yui-knk <spiketeika@gmail.com>
2019-01-05 12:04:57 +0900

    Add `dbname` to MiniAttributeRecord

--- src/catalog/mini_attribute.rs
+++ src/catalog/mini_attribute.rs
@@ -7,6 +7,8 @@ use catalog::catalog::Record;
 pub struct MiniAttributeRecord {
     // name of attribute
     name: String,
+    // name of db this attribute belongs to
+    dbname: String,
     // name of class this attribute belongs to
     class_name: String,
     type_name: String,
@@ -18,7 +20,7 @@ impl Record for MiniAttributeRecord {
     fn build_from_line(line: String) -> io::Result<Box<MiniAttributeRecord>> {
         let c: Vec<&str> = line.split(",").collect();
 
-        if c.len() != 4 {
+        if c.len() != 5 {
             return Err(Error::new(
                 ErrorKind::Other,
                 format!("Line ({}) is invalid.", line)
@@ -27,22 +29,30 @@ impl Record for MiniAttributeRecord {
 
         let r = MiniAttributeRecord {
             name: c[0].to_string(),
-            class_name: c[1].to_string(),
-            type_name: c[2].to_string(),
-            len: c[3].to_string().parse::<usize>().unwrap(),
+            dbname: c[1].to_string(),
+            class_name: c[2].to_string(),
+            type_name: c[3].to_string(),
+            len: c[4].to_string().parse::<usize>().unwrap(),
         };
         Ok(Box::new(r))
     }
 
     fn save_to_file(&self, w: &mut Write) -> io::Result<usize> {
-        w.write(format!("{},{},{},{}", self.name, self.class_name, self.type_name, self.len).as_bytes())
+        w.write(format!(
+            "{},{},{},{},{}",
+            self.name,
+            self.dbname,
+            self.class_name,
+            self.type_name,
+            self.len).as_bytes())
     }
 }
 
 impl MiniAttributeRecord {
-    pub fn new(name: String, class_name: String, type_name: String, len: usize) -> MiniAttributeRecord {
+    pub fn new(name: String, dbname: String, class_name: String, type_name: String, len: usize) -> MiniAttributeRecord {
         MiniAttributeRecord {
             name: name,
+            dbname: dbname,
             class_name: class_name,
             type_name: type_name,
             len: len
@@ -56,10 +66,11 @@ mod tests {
 
     #[test]
     fn test_record_build_from_line() {
-        let result1 = MiniAttributeRecord::build_from_line("id,table1,integer,4".to_string());
+        let result1 = MiniAttributeRecord::build_from_line("id,db2,table1,integer,4".to_string());
         assert_eq!(result1.is_ok(), true);
         let ok1 = result1.ok().unwrap();
         assert_eq!(ok1.name, "id".to_string());
+        assert_eq!(ok1.dbname, "db2".to_string());
         assert_eq!(ok1.class_name, "table1".to_string());
         assert_eq!(ok1.type_name, "integer".to_string());
         assert_eq!(ok1.len, 4);
@@ -72,12 +83,13 @@ mod tests {
     fn test_record_save_to_file() {
         let record = MiniAttributeRecord {
             name: "id".to_string(),
+            dbname: "db2".to_string(),
             class_name: "table1".to_string(),
             type_name: "integer".to_string(),
             len: 4,
         };
         let mut v = Vec::new();
         record.save_to_file(&mut v);
-        assert_eq!(v, b"id,table1,integer,4");
+        assert_eq!(v, b"id,db2,table1,integer,4");
     }
 }
