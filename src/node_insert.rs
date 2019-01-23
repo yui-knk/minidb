43515a8f1f0f5fc6143b6494f6af443eb4505fd4
yui-knk <spiketeika@gmail.com>
2019-01-23 10:09:37 +0900

    Ignore some warnings

--- src/node_insert.rs
+++ src/node_insert.rs
@@ -1,3 +1,5 @@
+#![allow(non_snake_case)]
+
 use std::cell::RefCell;
 
 use tuple::{TupleTableSlot};
--- src/off.rs
+++ src/off.rs
@@ -2,5 +2,6 @@
 // this is a 0-based index into the linp (ItemIdData) array in the
 // header of each disk page.
 
+#![allow(non_upper_case_globals)]
 pub type OffsetNumber = u16;
 pub const FirstOffsetNumber: OffsetNumber = 0;
